f1866fab5f2fe68f45e86edf825af1b2284383ed
sapar6ek0v <razetka0309@gmail.com>
2023-04-30 18:37:00 +0300

    disable cursor

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -5,14 +5,15 @@ const Cursor = () => {
   const { variants, cursorVariant, spring, text } = useMousePosition();
 
   return (
-    <CustomCursor
-      variants={variants}
-      className="circle"
-      animate={cursorVariant}
-      transition={spring}
-    >
-      {text && <span className="cursorText">{text}</span>}
-    </CustomCursor>
+    // <CustomCursor
+    //   variants={variants}
+    //   className="circle"
+    //   animate={cursorVariant}
+    //   transition={spring}
+    // >
+    //   {text && <span className="cursorText">{text}</span>}
+    // </CustomCursor>
+    <div></div>
   );
 };
 
