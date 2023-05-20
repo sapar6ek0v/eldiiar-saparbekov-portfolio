fea7df82a80f7365d25ec22a51aba1260aa14657
sapar6ek0v <razetka0309@gmail.com>
2023-05-20 22:34:23 +0300

    add "SEO component"

---
+++ src/components/SEO/index.tsx
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import { FC } from 'react';
+
+interface Props {
+  title: string;
+  description: string;
+  name: string;
+  type: string;
+  keywords: string;
+  href: string;
+}
+
+const SEO: FC<Props> = ({ title, description, name, type, keywords, href }) => {
+  return (
+    <Head>
+      {/* Standard metadata tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} data-rh="true" />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={name} />
+      <meta name="theme-color" content=" #212121" />
+      <meta name="robots" content="index, follow" />
+      {/* End standard metadata tags */}
+      {/* Facebook tags */}
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {/* End Facebook tags */}
+      {/* Twitter tags */}
+      <meta name="twitter:creator" content={name} />
+      <meta name="twitter:card" content={type} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {/* End Twitter tags */}
+      <link rel="canonical" href={href} />
+      <link rel='icon' href='/logo.png' />
+    </Head>
+  );
+};
+
+export default SEO;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
 import { lazy, Suspense, useState, useEffect } from 'react';
 
 import Loader from '../components/Loader';
+import SEO from '../components/SEO';
 
 const Layout = lazy(() => import('../components/Layout'));
 const ProfileImage = lazy(() => import('../components/ProfileImage'));
@@ -22,11 +22,56 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Eldiiar Saparbekov</title>
-        <meta name='description' content='Eldiiar Saparbekov personal website' />
-        <link rel='icon' href='/logo.png' />
-      </Head>
+      <SEO
+        title="Eldiiar Saparbekov"
+        description={`
+          Welcome to my world of frontend development!
+
+          Prepare to be captivated as you explore my portfolio, showcasing my skills and passion for creating stunning digital experiences. Step into a realm where creativity and innovation blend seamlessly to deliver exceptional frontend solutions.
+        
+          As you enter my website, you'll be greeted by a visually striking interface that reflects my unique style and attention to detail. The sleek design and thoughtfully chosen color palette create a harmonious visual experience, immediately drawing you into my world.
+        
+          On the homepage, I invite you to dive into my journey. A concise yet engaging introduction sets the tone for what lies ahead. Immerse yourself in a collection of meticulously curated projects and an awe-inspiring gallery that showcases my mastery of frontend development. From dynamic web designs to intuitive user interfaces, each project exemplifies my ability to transform ideas into visually stunning digital realities.
+        
+          Navigating through the site is an effortless experience. The intuitive menu effortlessly guides you through different sections, allowing you to explore my expertise in HTML, CSS, JavaScript, and my proficiency in popular frameworks like React and Next.js. Delve into detailed insights and explore examples of my work, providing you with a comprehensive understanding of my skills and capabilities.
+        
+          But I don't just want to showcase the end results. I believe in sharing the story behind each project. Through captivating case studies, I invite you to join me on the journey of creation. Gain a firsthand glimpse into my problem-solving mindset, as I tackle challenges head-on and implement innovative solutions. It's a testament to my meticulous approach and commitment to delivering top-notch frontend experiences.
+        
+          Connecting with me is a breeze. You'll find convenient access to my contact information, allowing potential collaborators, employers, or clients to reach out and explore exciting opportunities. Whether you have a question, a project proposal, or simply want to connect, I'm always ready to engage and discuss how we can bring your digital vision to life.
+        
+          Welcome to my frontend developer portfolio, where talent, creativity, and dedication converge to create unforgettable digital experiences. It's a window into my world, designed to inspire and ignite your own creative journey. Join me as we embark on a remarkable adventure in the realm of frontend development.
+        `}
+        name="Eldiiar Saparbekov - Frontend Developer"
+        type="portfolio website"
+        keywords="
+          Frontend Development,
+          Web Design,
+          User Interface (UI),
+          User Experience (UX),
+          HTML,
+          CSS,
+          JavaScript,
+          Responsive Design,
+          Web Development,
+          React,
+          Portfolio Showcase,
+          Case Studies,
+          Creative Solutions,
+          Innovative Designs,
+          Problem-solving,
+          Visual Storytelling,
+          Mobile-Friendly,
+          Cross-browser Compatibility,
+          Clean Code,
+          Collaboration,
+          Client Projects,
+          Professionalism,
+          Attention to Detail,
+          Contact Information
+        "
+        href='/'
+      />
+
 
       <Suspense fallback={<Loader />} >
         {
