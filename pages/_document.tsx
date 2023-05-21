c17db7f4c424b9d401aaa035170843f792179688
sapar6ek0v <razetka0309@gmail.com>
2023-05-21 10:05:30 +0300

    update file folders

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document,
+{
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -28,5 +34,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     };
+
   };
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta name="google-site-verification" content="RaMy2C81mDtN68q8CBPGJKrePdeSmYRKbvfUXzPgKL4" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 };
